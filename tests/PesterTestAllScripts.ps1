9095d11dd8c1d5315fea38c774f4e7372daad843
Mark Lewis <mark.lewis@jouzu.co.uk>
2019-10-17 13:08:49 +0100

    Update PesterTestAllScripts.ps1

--- tests/PesterTestAllScripts.ps1
+++ tests/PesterTestAllScripts.ps1
@@ -6,7 +6,7 @@
 $script:moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
 $script:globalRoot = Split-Path -Parent $script:moduleRoot
 $psScripts = Get-ChildItem $script:moduleRoot -Filter '*.ps1' -Recurse | Where-Object {$_.FullName -notmatch 'ps-test-framework'}
-$unitTests = $psScripts | Where-Object { $_.FullName -match '\Unit\' }
+$unitTests = $psScripts | Where-Object { $_.FullName -match '\\Unit\\' }
 
 
 foreach ($unitTest in $unitTests)
@@ -15,7 +15,7 @@ foreach ($unitTest in $unitTests)
     $unitTestName = $unitTest.Name
     $unitTestName = $unitTestName.TrimEnd('.Tests.ps1')
 
-    $scriptToTest = $psScripts | Where-Object { $_.FullName -notmatch '\unit\' -and $_.Name -match $unitTestName }
+    $scriptToTest = $psScripts | Where-Object { $_.FullName -notmatch '\\unit\\' -and $_.Name -match $unitTestName }
 
     Invoke-Pester $unitTestFullPath -CodeCoverage $scriptToTest
 }
